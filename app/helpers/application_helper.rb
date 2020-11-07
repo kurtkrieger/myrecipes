29f2990c3a68aac2fd4452e078e545378204e69e
Kurt Krieger <kurt.krieger.retail@outlook.com>
2020-11-07 05:35:26 +0000

    Add flash

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,13 @@ module ApplicationHelper
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     image_tag(gravatar_url, alt: user.chefname, class: "img-circle")
   end
+  
+  def flash_class(level)
+    case level
+      when "notice" then "alert alert-info"
+      when "success" then "alert alert-success"
+      when "error" then "alert alert-error"
+      when "alert" then "alert alert-error"
+    end
+  end
 end
