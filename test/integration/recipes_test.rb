bbedbd1fb99a23350205486f120628252c2c02e5
Kurt Krieger <kurt.krieger.retail@outlook.com>
2020-11-08 06:42:03 +0000

    Refactor

--- test/integration/recipes_test.rb
+++ test/integration/recipes_test.rb
@@ -28,6 +28,7 @@ class RecipesTest < ActionDispatch::IntegrationTest
     assert_match @chef.chefname, response.body
     assert_select "a[href=?]", edit_recipe_path(@recipe1), text: "Edit this recipe"
     assert_select "a[href=?]", recipe_path(@recipe1), text: "Delete this recipe"
+    assert_select "a[href=?]", recipes_path, text: "Return to recipe listing"
   end
   
   test "valid recipe create" do
