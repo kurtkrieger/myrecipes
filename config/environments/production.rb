942032295c65ce29b3fe1d95cad3027cc9116fa2
Kurt Krieger <kurt.krieger.retail@outlook.com>
2020-11-28 03:00:33 +0000

    Add redis 2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,5 +84,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   config.web_socket_server_url = "wss://kek-myrecipes.herokuapp.com/cable"
-  config.action_cable.allowed_request_origin = ["http://kek-myrecipes.herokuapp.com", "https://kek-myrecipes.herokuapp.com"]
+  config.action_cable.allowed_request_origins = ["http://kek-myrecipes.herokuapp.com", "https://kek-myrecipes.herokuapp.com"]
 end
